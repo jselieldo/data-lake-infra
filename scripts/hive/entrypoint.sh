41891c01d70732a18258ce1a7c32701e31a49869
Jose Elieldo <joseelieldo2@gmail.com>
2024-12-02 00:15:31 -0300

    "Atualizacao Hive Metastore"

--- scripts/hive/entrypoint.sh
+++ scripts/hive/entrypoint.sh
@@ -3,6 +3,21 @@
 # Exit on any error
 set -e
 
+echo "Substituindo variáveis no arquivo hive-site.xml..."
+
+sed -i "s|\${DB_HOST}|$DB_HOST|g" /opt/hive/conf/hive-site.xml
+sed -i "s|\${DB_PORT}|$DB_PORT|g" /opt/hive/conf/hive-site.xml
+sed -i "s|\${DB_NAME}|$DB_NAME|g" /opt/hive/conf/hive-site.xml
+sed -i "s|\${DB_USER}|$DB_USER|g" /opt/hive/conf/hive-site.xml
+sed -i "s|\${DB_PASSWORD}|$DB_PASSWORD|g" /opt/hive/conf/hive-site.xml
+sed -i "s|\${S3_ACCESS_KEY}|$S3_ACCESS_KEY|g" /opt/hive/conf/hive-site.xml
+sed -i "s|\${S3_SECRET_KEY}|$S3_SECRET_KEY|g" /opt/hive/conf/hive-site.xml
+
+echo "Substituindo variáveis no arquivo core-site.xml..."
+
+sed -i "s|\${S3_ACCESS_KEY}|$S3_ACCESS_KEY|g" /opt/hive/conf/core-site.xml
+sed -i "s|\${S3_SECRET_KEY}|$S3_SECRET_KEY|g" /opt/hive/conf/core-site.xml
+
 echo "Initializing Hive schema..."
 /opt/hive/bin/schematool -dbType mysql -initSchema || echo "Schema already initialized."
 
