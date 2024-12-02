9d65b6640876afe11d35d0c5e49e339155aed99a
Jose Elieldo <joseelieldo2@gmail.com>
2024-12-02 02:49:15 -0300

    "Ajuste do container Spark"

--- scripts/download_jars.sh
+++ scripts/download_jars.sh
@@ -11,5 +11,7 @@ wget -P "$destino" https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3
 wget -P "$destino" https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.1026/aws-java-sdk-bundle-1.11.1026.jar
 wget -P "$destino" https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.3.0/mysql-connector-j-8.3.0.jar
 wget -P "$destino" https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-hive-runtime/1.4.0/iceberg-hive-runtime-1.4.0.jar
+wget -P "$destino" https://repo.maven.apache.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar
+wget -P "$destino" https://repo.maven.apache.org/maven2/org/apache/hadoop/hadoop-common/3.3.4/hadoop-common-3.3.4.jar
 
 echo "Arquivos baixados com sucesso!"
